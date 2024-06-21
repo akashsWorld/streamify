372dd06a2c19a39e48dcbd3020c4aaee254e21bb
Akash Biswas <akashbiswas.271999@gmail.com>
2024-06-22 03:09:06 +0530

    Implement the user serializer.

--- server/user/urls.py
+++ server/user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import UserView
 
 urlpatterns = [
-    path('', UserView.as_view())
+    path('', UserView.as_view()),
+    path('authenticate/', UserView.as_view())
 ]
--- server/user/views.py
+++ server/user/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import UserSerializer
 import os
 
 
 class UserView(APIView):
     def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid()
+        return Response('ok')
 
-        return Response()
+
