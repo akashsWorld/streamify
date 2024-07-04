6738d7976a4f6b9f544730a1d68e79baa688a762
Akash Biswas <akashbiswas.271999@gmail.com>
2024-07-04 17:02:36 +0530

    The Homescreen is ready but the bg video is too large so that have to remove from version Control.

--- client/src/app/pages/videos/videos.component.ts
+++ client/src/app/pages/videos/videos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-videos',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './videos.component.html'
 })
 export class VideosComponent {
+  @ViewChild('billBoard', { static: false }) billBoard: ElementRef|undefined= undefined;
 
+  playVideo() {
+    console.log('Hello ')
+    console.log(this.billBoard)
+    if(this.billBoard)
+    this.billBoard.nativeElement.play();
+  }
 }
